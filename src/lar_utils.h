42dff11a4444ae03bd1cce85a4730524b629f897
SergioMedinaNieto <160664213+SergioMedinaNieto@users.noreply.github.com>
2025-07-07 14:35:04 +0200

    Update lar_utils.h

--- src/lar_utils.h
+++ src/lar_utils.h
@@ -7,52 +7,47 @@
 #include <vector>
 #include <iostream>
 
-extern double calcularMedia(std::vector<double> vec);
-
-extern double EfieldAtStep(double x, double y, double z);
-
-// Constantes
 extern double fWion;
 extern double fWph;
-//extern double Efield;
 extern double fScintPreScale;
-
-extern bool LArQL;
-
+extern double LAr_rho; 
 extern double Efield;
 
 extern double fLarqlChi0A;
 extern double fLarqlChi0B;
 extern double fLarqlChi0C;
 extern double fLarqlChi0D;
-
 extern double fLarqlAlpha;
 extern double fLarqlBeta;
 
-extern double LAr_rho; 
-
-extern bool fUseEllipsModBoxRecomb;
-extern double fEllipsModBoxA;
-extern double fEllipsModBoxB;
-extern double fEllipsModBoxR;
-
-extern bool fUseBinomialFlucts;
-
-extern bool fUseModBoxRecomb;
 extern double fRecombA;
 extern double fRecombk;
 
 extern double fModBoxA;
 extern double fModBoxB;
 
+extern double fEllipsModBoxA;
+extern double fEllipsModBoxB;
+extern double fEllipsModBoxR;
+
 extern double fQAlpha;
 extern double fQProton;
 
-// Funciones
-double AngleToEFieldStep(double Efield, double x0, double y0, double z0, double x1, double y1, double z1);
-double fModBoxRecomb(double Efield, double dEdx);
-double fEscapingEFraction(double dEdx);
-double fFieldCorrection(double Efield, double dEdx);
-double fBirks(double Efield, double dEdx);
+extern bool LArQL;
+extern bool fUseBinomialFlucts;
+extern bool fUseModBoxRecomb;
+extern bool fUseEllipsModBoxRecomb;
+
+// 
+extern double calcularMedia(std::vector<double> vec);
+extern double EfieldAtStep(double x, double y, double z);
+extern double AngleToEFieldStep(double Efield, double x0, double y0, double z0, double x1, double y1, double z1);
+
+extern double fBirks(double Efield, double dEdx);
+extern double fModBoxRecomb(double Efield, double dEdx);
+extern double fEllipsModBoxRecomb(double Efield, double dEdx, double phi);
+
+extern double fEscapingEFraction(double dEdx);
+extern double fFieldCorrection(double Efield, double dEdx);
 
 #endif
